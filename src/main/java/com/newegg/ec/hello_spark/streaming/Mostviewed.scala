6e811d61d15f64ec53ef8a25a35b6193971d24cb
ricdong <dongtalk@gmail.com>
2015-04-26 15:25:24 +0800

    request count by the streaming dataframe

--- src/main/java/com/newegg/ec/hello_spark/streaming/Mostviewed.scala
+++ src/main/java/com/newegg/ec/hello_spark/streaming/Mostviewed.scala
@@ -7,12 +7,8 @@ import org.apache.spark.streaming.{Time, Seconds, StreamingContext}
 import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.{SparkContext, SparkConf}
-import org.apache.spark.sql.types.StructType
-import org.apache.spark.sql.types.StringType
-import org.apache.spark.sql.types.StructField
 
-import scala.collection.mutable.ArrayBuffer
-case class request(cs_uri_stem: String, cs_uri_query: String, c_ip: String)
+case class request(page: String, province: String, city: String)
 
 
 /**
@@ -21,26 +17,61 @@ case class request(cs_uri_stem: String, cs_uri_query: String, c_ip: String)
 object Mostviewed {
 
   def main(args: Array[String]) {
-    val args = Array("192.168.1.108", "mygrp-rd29-test", "us_truesight", "2") // production
+    val args = Array("192.168.1.108", "test", "us_truesight", "10") // production
     // val args = Array("10.16.40.35", "mygrp-rd29-test", "us_truesight", "2") // local test
 
     val Array(zkQuorum, group, topics, numThreads) = args
 
     val conf = new SparkConf().setAppName("most item viewed from truesight page").setMaster("local[2]")
 
-   // LoggerManager.setStreamingLogLevels(Level.ERROR)
+   LoggerManager.setStreamingLogLevels(Level.ERROR)
 
-    val ssc = new StreamingContext(conf, Seconds(30))
+    val ssc = new StreamingContext(conf, Seconds(3))
     val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
     // val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap, StorageLevel.MEMORY_ONLY).map(_._2)
     val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
 
-    val https: DStream[request] = lines.map(_.split("\u0001")).filter(x => (x.length > 19)).filter(x => x(7).equals("/Product/Product.aspx")).map { x => request(x(7), x(8), x(19))}
+    // lines.map(x => ("http:", x.split("\001")(0), x.split("\001").length)).print()
 
-    https.print()
-    //val winDStream : DStream[http_request] = https.window(Seconds(30 * 60), Seconds(2 * 60))
+    // val https = lines.map(_.split("\001")).map(x => request(x(6), x(74), x(75)))
+    val https = lines.map(_.split("\001")).filter(x => (x(75) != null && !x(75).equals(""))).map(x => request(x(6), x(74), x(75)))
+    // https.print(100)
 
-    ssc.start()
+    val winDStream : DStream[request] = https.window(Seconds(30 * 60), Seconds(60))
+
+    winDStream.foreachRDD((rdd: RDD[request], time: Time) => {
+
+      // Get the singleton instance of SQLContext
+      val sqlContext = SQLContextSingleton.getInstance(rdd.sparkContext)
+      import sqlContext.implicits._
+
+      // Convert RDD[String] to RDD[case class] to DataFrame
+      val wordsDataFrame = rdd.toDF()
+
+      // Register as table
+      wordsDataFrame.registerTempTable("requestfromwhere")
+
+      // Do word count on table using SQL and print it
+      val wordCountsDataFrame =
+        sqlContext.sql("select city, count(city) as t from requestfromwhere group by city order by t desc limit 10")
+      // println(s"========= $time =========")
+      wordCountsDataFrame.show()
+    })
+
+      ssc.start()
     ssc.awaitTermination()
+  }
+}
+
+/** Lazily instantiated singleton instance of SQLContext */
+object SQLSingleton {
+
+  @transient private var instance: SQLContext = _
+
+  def getInstance(sparkContext: SparkContext): SQLContext = {
+    if (instance == null) {
+      instance = new SQLContext(sparkContext)
+    }
+    instance
   }
 }
